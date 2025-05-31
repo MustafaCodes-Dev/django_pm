6e3ad7557e469b2da570e13099538ac2e32e8d6a
Mustafa Ali <mustafa2025_19@hotmail.com>
2025-05-31 05:03:26 +0300

    create and list projects

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
-    
+    path('',views.ProjectListView.as_view(), name= 'Project_List'),
+    path('project/create',views.ProjectCreateView.as_view(), name= 'Project_Create'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from . import models
+from django.views.generic import ListView , CreateView
+from . import forms
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class ProjectListView(ListView):
+    model= models.Project
+    template_name = 'project/list.html'
+    context_object_name = 'projects'
+
+class ProjectCreateView(CreateView):
+    model= models.Project
+    form_class= forms.ProjectCreateForm
+    template_name= 'project/create.html'
+    success_url= reverse_lazy('Project_List')
+
+
+
