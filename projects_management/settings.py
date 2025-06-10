627b2b116d62b6da4ead58f99bc228b9b4c99e01
Mustafa Ali <mustafa2025_19@hotmail.com>
2025-06-11 02:03:17 +0300

    Search and Pagination

--- projects_management/settings.py
+++ projects_management/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'debug_toolbar',
     'projects.apps.ProjectsConfig',
     'accounts.apps.AccountsConfig',
 ]
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'projects_management.urls'
@@ -140,3 +142,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS=[
+    
+    '127.0.0.1',
+    
+]
