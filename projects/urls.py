0d2312a22b593b2406aa412202f7a3a4c5f06bcc
Mustafa Ali <mustafa2025_19@hotmail.com>
2025-06-14 02:50:54 +0300

    Authorization

--- projects/urls.py
+++ projects/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path('project/create',views.ProjectCreateView.as_view(), name= 'Project_Create'),
     path('project/edit/<int:pk>',views.ProjectUpdateView.as_view(), name= 'Project_update'),
     path('project/delete/<int:pk>',views.ProjectDeleteView.as_view(), name= 'Project_delete'),
-    path('project/task/create',views.TaskCreateView.as_view(), name= 'Task_create'),
-    path('project/task/edit/<int:pk>',views.TaskUpdateView.as_view(), name= 'Task_update'),
+    path("task/create/<int:project_id>/", views.TaskCreateView.as_view(), name="Task_create"),    path('project/task/edit/<int:pk>',views.TaskUpdateView.as_view(), name= 'Task_update'),
     path('project/task/delete/<int:pk>',views.TaskDeleteView.as_view(), name= 'Task_delete'),
 
 ]
--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,7 @@ from . import models
 from django.views.generic import ListView , CreateView, UpdateView , DeleteView
 from . import forms
 from django.urls import reverse_lazy , reverse
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin , UserPassesTestMixin
 
 
 class ProjectListView(LoginRequiredMixin, ListView):
@@ -11,9 +11,10 @@ class ProjectListView(LoginRequiredMixin, ListView):
     template_name = 'project/list.html'
     context_object_name = 'projects'
     paginate_by= 6
+
     def get_queryset(self):
         query_set= super().get_queryset()
-        where= {}
+        where= {'user_id':self.request.user}
         q = self.request.GET.get('q' , None)
         if q:
             where['title__icontains'] = q
@@ -26,34 +27,60 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
     template_name= 'project/create.html'
     success_url= reverse_lazy('Project_List')
 
-class ProjectDeleteView(LoginRequiredMixin, DeleteView):
+    def form_valid(self, form):
+        form.instance.user= self.request.user
+        return super().form_valid(form)
+    
+
+class ProjectDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView ):
     model= models.Project
     template_name= 'project/delete.html'
     success_url= reverse_lazy('Project_List')
-
-class ProjectUpdateView(LoginRequiredMixin, UpdateView,):
+    def test_func(self):
+        return self.get_object().user_id == self.request.user.id
+class ProjectUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView,):
     model= models.Project
     form_class= forms.ProjectUpdateForm
     template_name= 'project/update.html'
     
+    def test_func(self):
+        return self.get_object().user_id == self.request.user.id
+    
     def get_success_url(self):
         return reverse('Project_update', args= [self.object.id])
     
-class TaskCreateView(LoginRequiredMixin, CreateView):
-    model=models.Task
-    fields= ['project','description']
+class TaskCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = models.Task
+    fields = ['description']  # لا نعرض project للمستخدم
+
     def get_success_url(self):
-        return reverse('Project_update', args= [self.object.project.id])
+        return reverse('Project_update', args=[self.kwargs['project_id']])
+
+    def form_valid(self, form):
+        # اربط المهمة بالمشروع قبل الحفظ
+        project = models.Project.objects.get(pk=self.kwargs['project_id'])
+        form.instance.project = project
+        return super().form_valid(form)
+
+    def test_func(self):
+        # تحقّق من أن المشروع يخص المستخدم الحالي
+        project = models.Project.objects.get(pk=self.kwargs['project_id'])
+        return project.user == self.request.user
     
-class TaskUpdateView(LoginRequiredMixin, UpdateView):
+class TaskUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
     model=models.Task
     fields= ['is_completed']
     http_method_names = ['post']
     def get_success_url(self):
         return reverse('Project_update', args= [self.object.project.id])
     
-class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    def test_func(self):
+        return self.get_object().project.user_id == self.request.user.id
+    
+class TaskDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
     model=models.Task
+    def test_func(self):
+        return self.get_object().project.user_id == self.request.user.id
 
     def get_success_url(self):
         return reverse('Project_update', args= [self.object.project.id])
