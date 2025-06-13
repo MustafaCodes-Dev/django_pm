ae1ec9c4b8b46b43989667d10fce3b81421f6c60
Mustafa Ali <mustafa2025_19@hotmail.com>
2025-06-13 21:15:00 +0300

    Registeration & Profile update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path , include
 from django.contrib.auth.views import LoginView
-from . import forms
+from . import forms , views
+
 
 urlpatterns = [
     path('login/',LoginView.as_view(authentication_form =forms.UserLoginForm),name='login'),
+    path('register/',views.RegisterView.as_view(),name='register'),
+    path('profile/',views.edit_profile,name='profile'),
     path('', include('django.contrib.auth.urls')),
 ]
